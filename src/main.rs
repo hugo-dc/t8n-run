6b39d1d84e6d4b96442cd0547850ef5e4da81332
Hugo De la cruz <jhugodc@gmail.com>
2022-10-31 11:04:49 -0700

    Implement run

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ use std::fs;
 use std::io;
 use std::io::Write;
 use std::path::Path;
+use std::process::Command;
 use std::io::BufReader;
 use std::collections::HashMap;
 use serde_json::{Result, Value};
 use serde::{Deserialize, Serialize};
+use std::ffi::OsStr;
 use home;
+
+
+// utils
+fn hex_remove_leading_zero(hex: String) -> String {
+  if hex.starts_with("0x0") {
+    String::from("0x") + &hex[3..]
+  } else {
+    hex
+  }
+}
 
 #[derive(Deserialize, Debug, Clone)]
 struct Info {
@@ -23,21 +35,21 @@ struct Info {
     source_hash : String
 }
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Env {
-    #[serde(alias="currentBaseFee")]
+    #[serde(rename="currentBaseFee")]
     current_base_fee : String,
-    #[serde(alias="currentCoinbase")]
+    #[serde(rename="currentCoinbase")]
     current_coinbase : String,
-    #[serde(alias="currentDifficulty")]
+    #[serde(rename="currentDifficulty")]
     current_difficulty : String,
-    #[serde(alias="currentGasLimit")]
+    #[serde(rename="currentGasLimit")]
     current_gas_limit : String,
-    #[serde(alias="currentNumber")]
+    #[serde(rename="currentNumber")]
     current_number : String,
-    #[serde(alias="currentTimestamp")]
+    #[serde(rename="currentTimestamp")]
     current_timestamp : String,
-    #[serde(alias="previousHash")]
+    #[serde(rename="previousHash")]
     previous_hash : String
 }
 
@@ -70,11 +82,12 @@ struct Post {
     txbytes: String
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct TransactionT8n {
     input : String,
     gas: String,
-    #[serde(alias="gasPrice")]
+    #[serde(rename="gasPrice")]
     gas_price: String,
     nonce: String,
     to: String,
@@ -82,12 +95,37 @@ struct TransactionT8n {
     v: String,
     r: String,
     s: String,
-    #[serde(alias="gasPrice")]
+    #[serde(rename="secretKey")]
     secret_key: String,
-    #[serde(alias="chainId")]
+    #[serde(rename="chainId")]
     chain_id : String,
-    #[serde(alias="type")]
+    #[serde(rename="type")]
     tx_type: Option<String>
+}
+
+impl TransactionT8n {
+  pub fn new(input: String, gas: String, gas_price: String, nonce: String, to: String, value: String, secret_key: String, tx_type: Option<String>) -> TransactionT8n {
+
+    let gas = hex_remove_leading_zero(gas);
+    let gas_price = hex_remove_leading_zero(gas_price);
+    let nonce = hex_remove_leading_zero(nonce);
+    let value = hex_remove_leading_zero(value);
+
+    return TransactionT8n {
+      input,
+      gas,
+      gas_price,
+      nonce,
+      to,
+      value,
+      v: String::from("0x0"),
+      r: String::from("0x0"),
+      s: String::from("0x0"),
+      secret_key: secret_key,
+      chain_id: String::from("0x1"),
+      tx_type
+    }
+  }
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -115,14 +153,14 @@ struct StateTestContent {
     transaction: Transaction
 }
 
-struct Command {
+struct ReplCommand {
     command_name: String,
     command_params: Vec<String>,
 }
 
-impl Command {
-    pub fn from_string(st : String) -> Option<Command> {
-        let valid_commands = vec!["exit", "help", "extract", "dir", "alloc", "env", "txs", "hf"];
+impl ReplCommand {
+    pub fn from_string(st : String) -> Option<ReplCommand> {
+        let valid_commands = vec!["exit", "help", "extract", "dir", "alloc", "env", "txs", "hf", "run", "t8n", "evm"];
 
         let input_command = st.clone();
         let input_command = input_command.trim();
@@ -142,7 +180,7 @@ impl Command {
         }
 
         if valid_commands.iter().find(|&&s| s == cmd.as_str()).is_some() {
-           return Some(Command { command_name : cmd, command_params: params });
+           return Some(ReplCommand { command_name : cmd, command_params: params });
         }
         return None;
     }
@@ -153,7 +191,7 @@ impl Command {
         println!("\textract <test>\t\tExtract context information from Ethereum State Test");
         println!("\thf <hf_name>\tSet HardFork");
         println!("\tt8n <t8n path>\t\tSet t8n tool path");
-        println!("\tt8nFlag <t8n_flag>\tSet t8n tool flag (if needed)");
+        println!("\tevm <evm_path>\tSet custom EVMC vm");
         println!("\talloc\t\t\tShow current allocation data");
         println!("\tenv\t\t\tShow current environment");
         println!("\ttxs\t\t\tShow current transactions");
@@ -190,20 +228,15 @@ impl Command {
 
             for (i, d) in transaction.data.iter().enumerate() {
                 let tx_st = transaction.clone();
-                let tx_ctx = TransactionT8n {
-                    input: d.to_string(),
-                    gas: transaction.gas_limit[i].clone(),
-                    gas_price: tx_st.gas_price,
-                    nonce: tx_st.nonce,
-                    to: tx_st.to,
-                    value: transaction.value[i].clone(),
-                    v: String::from("0x0"),
-                    r: String::from("0x0"),
-                    s: String::from("0x0"),
-                    secret_key: tx_st.secret_key,
-                    chain_id: String::from("0x1"),
-                    tx_type: Some(String::from("0x1"))
-                };
+                let tx_ctx = TransactionT8n::new(
+                  d.to_string(), 
+                  transaction.gas_limit[i].clone(), 
+                  tx_st.gas_price, 
+                  tx_st.nonce, 
+                  tx_st.to, 
+                  transaction.value[i].clone(), 
+                  tx_st.secret_key, 
+                  None);
 
                 txs.push(tx_ctx);
             }
@@ -240,6 +273,90 @@ impl Command {
     }
 
     fn cmd_run(&self, ctx: &mut Context) {
+      let work_dir = ctx.config.work_dir.clone();
+
+      // Save json files
+      let alloc = ctx.alloc.clone();
+      let alloc_str = serde_json::to_string(&alloc).unwrap();
+      let alloc_file_path = work_dir.clone() + "/alloc.json";
+      let mut file = fs::File::create(alloc_file_path).unwrap();
+      file.write(alloc_str.as_bytes()).expect("Error writing alloc.json file");
+
+
+      let env = ctx.env.clone();
+      let env_str = serde_json::to_string(&env).unwrap();
+      let env_file_path = work_dir.clone() + "/env.json";
+      let mut file = fs::File::create(env_file_path).unwrap();
+      file.write(env_str.as_bytes()).expect("Error writing env.json file");
+
+      let txs = ctx.txs.clone();
+      let txs_str = serde_json::to_string(&txs).unwrap();
+      let txs_file_path = work_dir.clone() + "/txs.json";
+      let mut file = fs::File::create(txs_file_path).unwrap();
+      file.write(txs_str.as_bytes()).expect("Error writing txs.json file");
+
+      // Execute t8n tool
+      let fork_flag = String::from("--state.fork=") + ctx.config.hard_fork.as_str();
+      let alloc_flag = String::from("--input.alloc=") + work_dir.as_str() + "/alloc.json";
+      let env_flag = String::from("--input.env=") + work_dir.as_str() + "/env.json";
+      let txs_flag = String::from("--input.txs=") + work_dir.as_str() + "/txs.json";
+      let result_flag = String::from("--output.result=") + "alloc_jsontx.json";
+      let body_flag = String::from("--output.body=") + "/signed_txs.rlp";
+      let basedir_flag = String::from("--output.basedir=") + work_dir.as_str();
+      let trace_flag = String::from("--trace");
+
+      let mut args: Vec<String> = Vec::new();
+
+      if ctx.config.evm.as_str() != "" {
+        let vm_flag = String::from("--vm.evm=") + ctx.config.evm.as_str();
+        args.push(vm_flag);
+      }
+
+      args.push("t8n".to_string());
+      args.push(fork_flag);
+      args.push(alloc_flag);
+      args.push(env_flag);
+      args.push(txs_flag);
+      args.push(result_flag);
+      args.push(body_flag);
+      args.push(basedir_flag);
+      args.push(trace_flag);
+
+      let mut cmd = Command::new(ctx.config.t8n.clone());
+      cmd.args(args);
+
+      let output = cmd.output().unwrap();
+      let output_text = String::from_utf8(output.stdout).unwrap();
+      let error_text = String::from_utf8(output.stderr).unwrap();
+      println!("{}", output_text);
+      println!("{}", error_text);
+
+      // TODO: Read trace output
+    }
+
+    fn cmd_set_t8n(&self, ctx: &mut Context) {
+      if self.command_params.len() != 1 {
+        println!("Error: 1 parameter expected (evm path)");
+        return;
+      }
+      ctx.config.t8n = self.command_params[0].clone();
+      ctx.config.save();
+      println!("Configured t8n tool {}", ctx.config.t8n);
+    }
+
+    fn cmd_set_evm(&self, ctx: &mut Context) {
+      if self.command_params.len() != 1 {
+        println!("Error: 1 parameter expected (evm path)");
+        return;
+      }
+
+      if self.command_params[0] == "default" {
+        ctx.config.evm = String::from("");
+      } else {
+        ctx.config.evm = self.command_params[0].clone();
+      }
+      ctx.config.save();
+      println!("Configured evm {}", ctx.config.evm);
     }
 
     pub fn execute(&self, ctx : &mut Context) -> bool {
@@ -251,6 +368,8 @@ impl Command {
             "env" => ctx.print_env(),
             "txs" => ctx.print_txs(),
             "hf" => self.cmd_set_hard_fork(ctx),
+            "t8n" => self.cmd_set_t8n(ctx),
+            "evm" => self.cmd_set_evm(ctx),
             "run" => self.cmd_run(ctx),
             "exit" => return self.cmd_exit(),
             _ => self.cmd_unknown()
@@ -263,7 +382,7 @@ impl Command {
 struct Config {
     work_dir: String,
     t8n: String,
-    t8n_flag: String,
+    evm: String,
     hard_fork: String
 }
 
@@ -298,7 +417,7 @@ impl Config {
             let config = Config {
                work_dir : working_dir_path.to_str().unwrap().to_string(),
                t8n: String::from("/usr/bin/evm"),
-               t8n_flag : String::from(""),
+               evm : String::from(""),
                hard_fork: String::from("")
             };
 
@@ -322,7 +441,7 @@ impl Config {
 }
 
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Alloc {
     balance: String,
     code: String,
@@ -392,7 +511,7 @@ impl Repl {
             stdout.flush();
             stdin.read_line(&mut user_input)?;
 
-            let command = Command::from_string(user_input);
+            let command = ReplCommand::from_string(user_input);
 
             if command.is_some() {
                 let exit = command.unwrap().execute(&mut self.context);
@@ -409,7 +528,7 @@ impl Repl {
     fn welcome_message(&self) {
         println!("Welcome to t8n-repl");
         println!("Default working directory {}", self.context.config.work_dir);
-        println!("t8n tool: {} {}", self.context.config.t8n, self.context.config.t8n_flag);
+        println!("t8n tool: {} {}", self.context.config.t8n, self.context.config.evm);
     }
 }
 
