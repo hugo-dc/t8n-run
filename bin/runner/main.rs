6d6fc2c8d1ba3c37b648c45b15366c2b6a45f703
Hugo De la cruz <jhugodc@gmail.com>
2022-11-25 10:41:01 -0700

    Set HardFork from command line, add data and code

--- bin/runner/main.rs
+++ bin/runner/main.rs
@@ -7,6 +7,8 @@ struct Args {
     #[arg(short, long)]
     t8ntool: String,
     #[arg(short, long)]
+    data: Option<String>,
+    #[arg(short, long)]
     code: Option<String>,
     #[arg(short, long)]
     evm: Option<String>,
@@ -33,22 +35,19 @@ fn main() {
 
     let mut ctx = context_result.unwrap();
 
+    // Set Hard Fork
+    ctx.config.hard_fork = args.hard_fork.clone();
+
+    // Set EVM if provided
     if args.evm.is_some() {
         ctx.config.evm = args.evm.unwrap().to_string();
     }
 
-    if args.code.is_some() {
+    // Set code if provided
+    if args.code.is_some() || args.data.is_some() {
+        let _ = ctx.add_default_address(); 
         // Create sender account
         let snd_address = String::from("0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b");
-        let _ = ctx.add_default_address(); 
-
-        // Create receiver account
-        let rec_address = String::from("0x0000000000000000000000000000000000000100");
-        let _ = ctx.add_address(&rec_address);
-        let mut account: Alloc = ctx.alloc.get(&rec_address).unwrap().clone();
-        let _ = account.set_code(args.code.unwrap()); 
-        let _ = ctx.alloc.remove(&rec_address);
-        ctx.alloc.insert(rec_address.clone(), account.to_owned());
 
         // Create default transaction
         ctx.txs.push(TransactionT8n::default());
@@ -57,9 +56,24 @@ fn main() {
         let pk = ctx.get_secret_key(snd_address);
         ctx.txs[0].set_private_key(pk.unwrap().as_str());
 
-        // Set receiver
-        ctx.txs[0].set_receiver(&rec_address);
+        if args.code.is_some() {
+            // Create receiver account
+            let rec_address = String::from("0x0000000000000000000000000000000000000100");
+            let _ = ctx.add_address(&rec_address);
+            let mut account: Alloc = ctx.alloc.get(&rec_address).unwrap().clone();
+            let _ = account.set_code(args.code.unwrap()); 
+            let _ = ctx.alloc.remove(&rec_address);
+            ctx.alloc.insert(rec_address.clone(), account.to_owned());
+            // Set receiver
+            ctx.txs[0].set_receiver(&rec_address);
+        }
+
+        if args.data.is_some() {
+            // Set input data
+            ctx.txs[0].set_input(args.data.unwrap().as_str());
+        }
     }
+
 
     ctx.run();
 }
